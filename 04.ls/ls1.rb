c7b32453dd2e014c1d735c028eb9d05738016b9a
koyama-c <koyama@linkedge.jp>
2023-01-14 18:11:52 +0900

    ファイルとディレクトリで挙動を分ける

--- 04.ls/ls1.rb
+++ 04.ls/ls1.rb
@@ -6,12 +6,31 @@
 COLUMNS_NUMBER = 3
 
 def main
-  directory_path = ARGV.empty? ? '.' : ARGV[0]
-  other_than_hidden_files = Dir.each_child(directory_path).reject { |f| f.start_with?('.') }.sort
+  argv = ARGV.empty? ? ['.'] : ARGV
 
-  swap_rows_and_columns(COLUMNS_NUMBER, other_than_hidden_files).flatten.each.with_index(1) do |file, index|
+  argv.each do |file_path|
+    return puts "ruby ls1.rb: #{file_path}: No such file or directory" unless File.exist?(file_path)
+
+    puts "#{file_path}:" if argv.length != 1 && File.directory?(file_path)
+    File.directory?(file_path) ? print_files(file_path) : (puts file_path)
+    puts "\n" if argv.length != 1
+  end
+end
+
+def print_files(file_path)
+  other_than_hidden_files = Dir.each_child(file_path).reject { |f| f.start_with?('.') }.sort
+
+  rows_number = calc_rows(COLUMNS_NUMBER, other_than_hidden_files)
+  transposed_files = adjust_width(align_array_size(rows_number, other_than_hidden_files).each_slice(rows_number)).transpose
+
+  transposed_files.flatten.each.with_index(1) do |file, index|
     (index % COLUMNS_NUMBER).zero? ? (puts file) : (print file)
   end
+end
+
+# 配列の各要素のサイズを揃える
+def align_array_size(rows_number, files)
+  (files.length % rows_number).zero? ? files : files + Array.new(rows_number - files.length % rows_number, '')
 end
 
 # 行数の計算
@@ -25,16 +44,6 @@ def adjust_width(file_array)
     max_num = array.map(&:length).max
     new_array << array.map { |file| "#{file}#{"\s" * (max_num - file.length)}\s\s" }
   end
-end
-
-# 行と列を入れ替える
-def swap_rows_and_columns(num, files)
-  array = []
-  rows_number = calc_rows(num, files)
-  rows_number.times do |n|
-    array << adjust_width(files.each_slice(rows_number)).map { |f| f[n] }
-  end
-  array
 end
 
 main
