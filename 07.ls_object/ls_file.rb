767a13f198b5ac5f1570d33099a63db2be2b9e86
koyama-c <koyama@linkedge.jp>
2023-10-17 08:30:30 +0900

    重たい処理を何度も実行しないようにする

--- 07.ls_object/ls_file.rb
+++ 07.ls_object/ls_file.rb
@@ -17,47 +17,42 @@ class LsFile
   def initialize(directory_path:, file_name:)
     @file_path = "#{directory_path}/#{file_name}"
     @file_name = file_name
+    @stat = File.stat(@file_path.to_s)
   end
 
   def type
-    stat.ftype == 'file' ? '-' : stat.ftype[0]
+    @stat.ftype == 'file' ? '-' : @stat.ftype[0]
   end
 
   def permission
-    "#{PERMISSION[stat.mode.to_s(8)[-3]]}#{PERMISSION[stat.mode.to_s(8)[-2]]}#{PERMISSION[stat.mode.to_s(8)[-1]]}"
+    "#{PERMISSION[@stat.mode.to_s(8)[-3]]}#{PERMISSION[@stat.mode.to_s(8)[-2]]}#{PERMISSION[@stat.mode.to_s(8)[-1]]}"
   end
 
   def link_count
-    stat.nlink
+    @stat.nlink
   end
 
   def owner_name
-    Etc.getpwuid(stat.uid).name
+    Etc.getpwuid(@stat.uid).name
   end
 
   def group_name
-    Etc.getgrgid(stat.gid).name
+    Etc.getgrgid(@stat.gid).name
   end
 
   def byte_size
-    stat.size
+    @stat.size
   end
 
   def modify_time
-    stat.mtime
+    @stat.mtime
   end
 
   def blocks
-    stat.blocks
+    @stat.blocks
   end
 
   def extended_attributes
     `xattr "#{@file_path}"`
-  end
-
-  private
-
-  def stat
-    File.stat(@file_path.to_s)
   end
 end
