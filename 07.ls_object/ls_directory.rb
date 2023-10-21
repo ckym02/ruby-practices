5b655c72e9a3807b5a0ef798a8eced8c89441cf1
koyama-c <koyama@linkedge.jp>
2023-10-21 20:41:28 +0900

    重い処理をインスタンス変数に入れる

--- 07.ls_object/ls_directory.rb
+++ 07.ls_object/ls_directory.rb
@@ -3,23 +3,26 @@
 require_relative 'ls_file'
 
 class LsDirectory
+  attr_reader :ls_files
+
   def initialize(directory_path, hidden_file_presence: true, reversed_order: false)
     @directory_path = directory_path
     @hidden_file_presence = hidden_file_presence
     @reversed_order = reversed_order
+    @ls_files = prepare_ls_files
   end
 
-  def ls_files
+  def blocks_sum
+    @ls_files.sum(&:blocks)
+  end
+
+  private
+
+  def prepare_ls_files
     filtered_files = @hidden_file_presence ? directory_files : exclude_hidden_files
     ordered_files = @reversed_order ? filtered_files.sort.reverse : filtered_files.sort
     ordered_files.map { |file| LsFile.new(@directory_path, file) }
   end
-
-  def blocks_sum
-    ls_files.sum(&:blocks)
-  end
-
-  private
 
   def exclude_hidden_files
     directory_files.reject { |f| f.start_with?('.') }
